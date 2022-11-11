2b29dc1c50f455609543a11a2a192acacd5831ad
less <63160418+nextheroes@users.noreply.github.com>
2022-11-12 00:00:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import Reclassify as rc
-from Reclassify import *
+import ReClassify.ReClassify as rc
+import json
 
-data=rc.mqttHandler
-
-while(True):
-    rc.feed(data)
+def customMessage(mqttc, obj, msg):
+    data = json.loads(msg.payload.decode('utf-8'))
     print(data)
+client = rc(customMessage)
+client.go()
